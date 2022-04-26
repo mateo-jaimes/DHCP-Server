b48900c2039b3d771d9a71e481d7a974e4f61440
Nicolás Cubillos <79682493+nicolasdcubillos@users.noreply.github.com>
2022-04-26 18:35:23 -0500

    Cargado el proyecto base en Java.

---
+++ src/main/java/puj/redes/App.java
@@ -0,0 +1,13 @@
+package puj.redes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
