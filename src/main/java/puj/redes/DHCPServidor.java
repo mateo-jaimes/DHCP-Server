e7037c0563923070cc5060dc6f506a9107f9e6e7
Nicolas Cubillos <nicolasdavidcubillos@gmail.com>
2022-05-01 19:22:22 -0500

    Primer versión estable de servidor DHCP.

--- src/main/java/puj/redes/DHCPServidor.java
+++ src/main/java/puj/redes/DHCPServidor.java
@@ -10,8 +10,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
 import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -88,118 +86,115 @@ public class DHCPServidor {
         // Guardar en el archivo la solicitud.
 
         TipoMensajeDHCP tipoMensajeDHCP = TipoMensajeDHCP.INVALID, tipoRespuestaDHCP = TipoMensajeDHCP.INVALID;
-        byte[] IPsolicitada, IPservidor = new byte[4], subnet, hostname = new byte[0];
+        try {
+            byte[] IPsolicitada, IPservidor = new byte[4], subnet, hostname = new byte[0];
 
-        for (DHCPOpciones opcion : mensaje.getOpcionesDHCP()) {
-            switch (opcion.getType()) {
-                case 1:
-                    subnet = opcion.getValue();
-                    break;
+            for (DHCPOpciones opcion : mensaje.getOpcionesDHCP()) {
+                switch (opcion.getType()) {
+                    case 1:
+                        subnet = opcion.getValue();
+                        break;
 
-                case 12:
-                    hostname = opcion.getValue();
-                    break;
+                    case 12:
+                        hostname = opcion.getValue();
+                        break;
 
-                case 50:
-                    IPsolicitada = opcion.getValue();
-                    break;
+                    case 50:
+                        IPsolicitada = opcion.getValue();
+                        break;
 
-                case 53:
-                    tipoMensajeDHCP = TipoMensajeDHCP.values()[opcion.getValue()[0]];
-                    System.out.println("-> " + tipoMensajeDHCP);
-                    break;
+                    case 53:
+                        tipoMensajeDHCP = TipoMensajeDHCP.values()[opcion.getValue()[0]];
+                        System.out.println("-> " + tipoMensajeDHCP);
+                        break;
 
-                case 54:
-                    IPservidor = opcion.getValue();
-                    break;
+                    case 54:
+                        IPservidor = opcion.getValue();
+                        break;
+                }
             }
-        }
 
-        Registro registro = ControladorRegistros.buscarRegistro(mensaje.getChaddr(), mensaje.getHlen());
+            Registro registro = ControladorRegistros.buscarRegistro(mensaje.getChaddr(), mensaje.getHlen());
 
-        switch (tipoMensajeDHCP) {
-            case DHCPDISCOVER:
-                tipoRespuestaDHCP = TipoMensajeDHCP.DHCPOFFER;
-                break;
+            switch (tipoMensajeDHCP) {
+                case DHCPDISCOVER:
+                    tipoRespuestaDHCP = TipoMensajeDHCP.DHCPOFFER;
+                    break;
 
-            case DHCPREQUEST:
-                //System.out.println(Arrays.toString(IPservidor) + "-" + Arrays.toString(IPServidor.getAddress()));
-                if (InetAddress.getByAddress(IPservidor).equals(IPServidor))
-                    tipoRespuestaDHCP = TipoMensajeDHCP.DHCPACK;
-                else
-                    tipoRespuestaDHCP = TipoMensajeDHCP.DHCPNAK;
+                case DHCPREQUEST:
+                    //System.out.println(Arrays.toString(IPservidor) + "-" + Arrays.toString(IPServidor.getAddress()));
+                    if (InetAddress.getByAddress(IPservidor).equals(IPServidor))
+                        tipoRespuestaDHCP = TipoMensajeDHCP.DHCPACK;
 
-                break;
+                case DHCPDECLINE:
+                    break;
 
-            case DHCPDECLINE:
-                break;
+                case DHCPRELEASE:
+                    if (registro != null)
+                        ControladorRegistros.eliminarRegistro(registro);
+                    break;
 
-            case DHCPRELEASE:
-                if (registro != null)
-                    ControladorRegistros.eliminarRegistro(registro);
-                break;
+                case DHCPINFORM:
+                    if (registro != null)
+                        tipoRespuestaDHCP = TipoMensajeDHCP.DHCPACK;
+                    break;
 
-            case DHCPINFORM:
-                if (registro != null)
-                    tipoRespuestaDHCP = TipoMensajeDHCP.DHCPACK;
-                break;
+                default:
+                    return;
+            }
 
-            default:
+            if (tipoRespuestaDHCP == TipoMensajeDHCP.INVALID)
                 return;
-        }
 
-        responderCliente(tipoRespuestaDHCP, mensaje, new String(hostname), registro);
+            responderCliente(tipoRespuestaDHCP, mensaje, new String(hostname), registro);
+        } catch (Exception e) {
+            System.out.println("\t-> Error al procesar el mensaje " + tipoMensajeDHCP + ": "+ e.getMessage());
+        }
     }
-// FILTRO WIRESHARK udp.port == 68
-    public static void responderCliente (TipoMensajeDHCP tipoRespuestaDHCP, DHCPMensaje mensajeCliente, String hostname, Registro registro) throws IOException, ParseException {
+
+    public static void responderCliente (TipoMensajeDHCP tipoRespuestaDHCP, DHCPMensaje mensajeCliente, String hostname, Registro registro) throws Exception {
         InetAddress IPsolicitada;
         DHCPMensaje respuestaCliente = null;
         indexRespuestaMensaje = 0;
         ArrayList <DHCPOpciones> opcionesDHCP = new ArrayList<>();
 
-        switch (tipoRespuestaDHCP) { // verificar si se puede quitar el switch, por el ordinal del enum.
-            case DHCPNAK:
-                return;
-            case DHCPOFFER:
-                if (registro == null) {
-                    registro = new Registro(mensajeCliente.getChaddr(), obtenerPrimeraIPLibre(mensajeCliente.getYiaddr()), new Date(), new Date(), hostname); // Revisar dates.
-                    ControladorRegistros.anadirRegistro(registro);
-                }
-
-                Subred subred = obtenerSubred(mensajeCliente.getYiaddr());
-
-                // Opciones dentro del DHCP OFFER
-
-                opcionesDHCP.add(new DHCPOpciones((byte)53, new byte[]{(byte) tipoRespuestaDHCP.ordinal()})); // message type
-                opcionesDHCP.add(new DHCPOpciones((byte)54, IPServidor.getAddress())); // DHCP ip server
-                opcionesDHCP.add(new DHCPOpciones((byte)1, subred.getMascaraSubnet())); // mask subnet
-                opcionesDHCP.add(new DHCPOpciones((byte)3, subred.getPuertaEnlace())); // gateway
-                opcionesDHCP.add(new DHCPOpciones((byte)6, subred.getServidorDNS())); // IP servidor DNS
-                opcionesDHCP.add(new DHCPOpciones((byte)51, intToByte(subred.getTiempo()))); // revisar lease time.
-                opcionesDHCP.add(new DHCPOpciones((byte)59, intToByte((int) (subred.getTiempo() * 0.75)))); // revisar lease time.
-                opcionesDHCP.add(new DHCPOpciones((byte)58, intToByte((int) (subred.getTiempo() * 0.5)))); // revisar lease time.
-
-                respuestaCliente = new DHCPMensaje (
-                        (byte)2, // op
-                        (byte)1, // htype
-                        (byte)6, // hlen
-                        (byte)0, // hops
-                        mensajeCliente.getXid(), // xid
-                        (short)0, // secs
-                        ByteBuffer.wrap(new byte[] {(byte) 128, 0}).getShort(), // flags
-                        new byte[] {0, 0, 0, 0}, // ciaddr
-                        registro.getIP().getAddress(), // ip
-                        IPServidor.getAddress(), // ip sv
-                        new byte[] {0, 0, 0, 0}, // giaddr
-                        mensajeCliente.getChaddr(), // chaddr
-                        new byte[] {0}, // sname
-                        new byte[] {0}, // sfile
-                        opcionesDHCP
-                        );
-
-                System.out.println("IP " + registro.getIP() + " asignada a la MAC " + DHCPMensaje.printByteArray(registro.getChaddr(),2));
-                break;
+        if (registro == null) {
+            registro = new Registro(mensajeCliente.getChaddr(), obtenerPrimeraIPLibre(mensajeCliente.getYiaddr()), new Date(), new Date(), hostname); // Revisar dates.
+            ControladorRegistros.anadirRegistro(registro);
         }
+
+        Subred subred = obtenerSubred(mensajeCliente.getYiaddr());
+
+        // Opciones dentro de la respuesta DHCP
+
+        opcionesDHCP.add(new DHCPOpciones((byte) 53, new byte[]{(byte) tipoRespuestaDHCP.ordinal()})); // message type
+        opcionesDHCP.add(new DHCPOpciones((byte) 54, IPServidor.getAddress())); // DHCP ip server
+        opcionesDHCP.add(new DHCPOpciones((byte) 1, subred.getMascaraSubnet())); // mask subnet
+        opcionesDHCP.add(new DHCPOpciones((byte) 3, subred.getPuertaEnlace())); // gateway
+        opcionesDHCP.add(new DHCPOpciones((byte) 6, subred.getServidorDNS())); // IP servidor DNS
+        opcionesDHCP.add(new DHCPOpciones((byte) 51, intToByte(subred.getTiempo()))); // revisar lease time.
+        opcionesDHCP.add(new DHCPOpciones((byte) 59, intToByte((int) (subred.getTiempo() * 0.75)))); // revisar lease time.
+        opcionesDHCP.add(new DHCPOpciones((byte) 58, intToByte((int) (subred.getTiempo() * 0.5)))); // revisar lease time.
+
+        respuestaCliente = new DHCPMensaje(
+                (byte) 2, // op
+                (byte) 1, // htype
+                (byte) 6, // hlen
+                (byte) 0, // hops
+                mensajeCliente.getXid(), // xid
+                (short) 0, // secs
+                ByteBuffer.wrap(new byte[]{(byte) 128, 0}).getShort(), // flags
+                new byte[]{0, 0, 0, 0}, // ciaddr
+                registro.getIP().getAddress(), // ip
+                IPServidor.getAddress(), // ip sv
+                new byte[]{0, 0, 0, 0}, // giaddr
+                mensajeCliente.getChaddr(), // chaddr
+                new byte[]{0}, // sname
+                new byte[]{0}, // sfile
+                opcionesDHCP
+        );
+
+        System.out.println("IP " + registro.getIP() + " asignada a la MAC " + DHCPMensaje.printByteArray(registro.getChaddr(), 2));
         enviarRespuestaCliente(respuestaCliente, InetAddress.getByName("255.255.255.255"), tipoRespuestaDHCP); // poner el destination address...
     }
 
@@ -262,7 +257,7 @@ public class DHCPServidor {
         agregarBytesMensaje(new byte[] {(byte)255});
     }
 
-    private static InetAddress obtenerPrimeraIPLibre(byte[] clienteIP) throws UnknownHostException {
+    private static InetAddress obtenerPrimeraIPLibre(byte[] clienteIP) throws Exception {
         byte[] actual = obtenerSubred(clienteIP).getRangoInicial();
         byte[] ultima = obtenerSubred(clienteIP).getRangoFinal();
 
@@ -272,11 +267,10 @@ public class DHCPServidor {
             else
                 return InetAddress.getByAddress(actual);
 
-
-        return null;
+        throw new Exception ("No hay IP's disponibles para esta subred (gateway " + InetAddress.getByAddress(clienteIP) + ").");
     }
 
-    private static Subred obtenerSubred (byte[] clienteIP) {
+    private static Subred obtenerSubred (byte[] clienteIP) throws Exception {
         if (Arrays.equals(clienteIP, new byte[] {0, 0, 0, 0}))
             clienteIP = serverGateway.getAddress();
 
@@ -284,7 +278,8 @@ public class DHCPServidor {
             if (Arrays.equals(subred.getPuertaEnlace(), clienteIP))
                 return subred;
         }
-        return null;
+
+        throw new Exception ("No existe la subred dentro del archivo de configuraciones (gateway " + InetAddress.getByAddress(clienteIP) + ").");
     }
 
     private static boolean IPdisponible (byte[] IP) {
@@ -306,30 +301,4 @@ public class DHCPServidor {
             }
         }
     }
-
-    private static Subred obtenerSubredDesdeIP (byte[] IP) {
-        byte[] actual;
-        for (Subred subred : ControladorSubredes.getSubredes()) {
-            actual = subred.getRangoInicial();
-
-            while (!Arrays.equals(actual, subred.getRangoFinal())) {
-                if (!IPdisponible(actual)) {
-                    aumentarIP(actual);
-                } else return new Subred(); // arreglar.
-            }
-        }
-        return null;
-    }
-
-    /*
-    private static int compararIP(byte[] IP1, byte[] IP2)
-    {
-        for (int i = 0; i < 4; i++)
-            if (byteToInt(ip1[i]) > byteToInt(ip2[i]))
-                return 1;
-            else if (ByteBuffer.wrap(IP1[i]).getInt() < byteToInt(ip2[i]))
-                return -1;
-        return 0;
-    }
-     */
 }
